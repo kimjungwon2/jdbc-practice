3694e0a3ee4efb22e697440cda474569208ada3d
jungwon <mac2001035@gmail.com>
2024-07-12 01:05:18 +0900

    FEAT: 테스트 예제 생성.

---
+++ src/main/java/org/example/UserDao.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class UserDao {
+
+
+    public void create(User user) {
+
+    }
+
+    public User findByUserId(String wizard) {
+        return null;
+    }
+}
--- src/test/java/org/example/UserDaoTest.java
+++ src/test/java/org/example/UserDaoTest.java
@@ -1,9 +1,13 @@
 package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserDaoTest {
 
@@ -13,4 +17,17 @@ public class UserDaoTest {
         populator.addScript(new ClassPathResource("db_schema.sql"));
         DatabasePopulatorUtils.execute(populator, ConnectionManager.getDataSource());
     }
+
+    @DisplayName("")
+    @Test
+    void createTest(){
+      UserDao userDao = new UserDao();
+
+      userDao.create(new User("wizard","password","name","email"));
+
+      User user = userDao.findByUserId("wizard");
+      assertThat(user).isEqualTo(new User("wizard","password","name","email"));
+    }
+
+
 }
