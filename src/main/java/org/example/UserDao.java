9ccf19776b8b52301f6662db543c1dfcbba18b36
jungwon <mac2001035@gmail.com>
2024-07-14 19:36:00 +0900

    REFACTOR: PreparedStatement 익명 클래스 활용하여 분리.

--- src/main/java/org/example/UserDao.java
+++ src/main/java/org/example/UserDao.java
@@ -30,6 +30,20 @@ public class UserDao {
         }
     }
 
+    public void create2(User user) throws SQLException{
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "INSERT INTO USERS VALUES (?, ?, ?, ?)";
+        jdbcTemplate.executeUpdate(user,sql, new PreparedStatementSetter() {
+            @Override
+            public void setter(PreparedStatement pstmt) throws SQLException {
+                pstmt.setString(1, user.getUserId());
+                pstmt.setString(2, user.getPassword());
+                pstmt.setString(3, user.getName());
+                pstmt.setString(4, user.getEmail());
+            }
+        });
+    }
+
     public User findByUserId(String userId) throws SQLException {
         Connection con = null;
         PreparedStatement pstmt = null;
